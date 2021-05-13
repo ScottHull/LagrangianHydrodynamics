26de72eccda5de87fd9fd9da4a23ddd9e9caf79f
ScottHull <scottdhull21@gmail.com>
2021-05-13 17:23:39 -0400

    Initial commit.

--- src/initial_conditions.py
+++ src/initial_conditions.py
@@ -2,4 +2,6 @@
 
 """
 Initial atmosphere conditiosn described in Section 2.3 of Genda and Abe 2003.
+Assumes a hydrostatically equilibrated polytropic atmosphere with polytropic exponent gamma_a.
 """
+
