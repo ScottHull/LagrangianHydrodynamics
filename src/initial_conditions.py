857d4d2f1d7e51f09ea6be5871d90bf71125101e
ScottHull <scottdhull21@gmail.com>
2021-05-13 17:46:38 -0400

    Initial commit.

--- src/initial_conditions.py
+++ src/initial_conditions.py
@@ -5,3 +5,23 @@ Initial atmosphere conditiosn described in Section 2.3 of Genda and Abe 2003.
 Assumes a hydrostatically equilibrated polytropic atmosphere with polytropic exponent gamma_a.
 """
 
+def pressure_initial(polytropic_exponent, lambda_0, radius, radius_0):
+    a1 = ((polytropic_exponent - 1) / polytropic_exponent) * lambda_0
+    a2 = (radius_0 / radius) - 1
+    exponent = polytropic_exponent / (polytropic_exponent - 1)
+    p_p0 = (a1 * a2 + 1) ** exponent  # P / P_0
+    return p_p0
+
+def density_initial(polytropic_exponent, lambda_0, radius, radius_0):
+    a1 = ((polytropic_exponent - 1) / polytropic_exponent) * lambda_0
+    a2 = (radius_0 / radius) - 1
+    exponent = 1 / (polytropic_exponent - 1)
+    rho_rho0 = (a1 * a2 + 1) ** exponent  # rho / rho_0
+    return rho_rho0
+
+def temperature_initial(polytropic_exponent, lambda_0, radius, radius_0)
+    a1 = ((polytropic_exponent - 1) / polytropic_exponent) * lambda_0
+    a2 = (radius_0 / radius) - 1
+    exponent = 1
+    t_t0 = (a1 * a2 + 1) ** exponent  # T / T_0
+    return t_t0
